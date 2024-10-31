db9e3ceba9960a66c3e698baf41f8dacc076e74e
wijekim <130275773+wijekim@users.noreply.github.com>
2024-10-31 10:47:57 +0900

    Add files via upload

---
+++ 7-3/complex.hpp
@@ -0,0 +1,16 @@
+#ifndef COMPLEX_HPP_
+#define COMPLEX_HPP
+
+class Complex{
+    int real,img;
+public:
+    
+    Complex(int a=0, int b=0); //생성자
+    
+    Complex operator+(Complex CP); //+연산자 함수 선언
+    void show(); //출력해주는 함수
+
+};
+
+
+#endif
