791158c3c1123b6e37f59d77124c4883e9fdec63
wijekim <130275773+wijekim@users.noreply.github.com>
2024-11-04 16:00:03 +0900

    Add files via upload

---
+++ complex/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+#include "complex.hpp"
+
+int main() {
+    Complex x(2, 3);
+    ++x; //복소수 결과 증가
+    cout << "증가결과";
+    x.show();//증가된 결과 출력
+    x--; //복소수 결과 감소
+    cout << "감소결과";
+    x.show();//감소된 결과 출력
+}
