fc7689ec3a044c856d4204e72e42b502b3a18fd5
wijekim <130275773+wijekim@users.noreply.github.com>
2024-11-28 23:11:05 +0900

    Update two4.cpp

--- 10-5/test4/two4.cpp
+++ 10-5/test4/two4.cpp
@@ -6,14 +6,14 @@ int main(){
 
 
     cout<<"아홉개 정수를 입력하시오:";
-    for(int i=0;i<3;i++){
+    for(int i=0;i<3;i++){ //각위치에 정수입력
         for(int j=0;j<3;j++){
             cin>>score[i][j];
         }
     }
     cout<<endl;
 
-    for(int i=0;i<3;i++){
+    for(int i=0;i<3;i++){ //입력받은값 출력
         for(int j=0;j<3;j++){
             cout<<score[i][j]<<' ';
         }
@@ -24,15 +24,15 @@ int main(){
     
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(score[i][j]<0){
-                score[i][j] = 0;
+            if(score[i][j]<0){ //위치의 값이 0보다 작으면
+                score[i][j] = 0; //0
             }
             else
-                score[i][j] =255;
+                score[i][j] =255; //아니면 255
         }
     }
 
-    for(int i=0;i<3;i++){
+    for(int i=0;i<3;i++){ //변형된값 출력
         for(int j=0;j<3;j++){
             cout<<score[i][j]<<' ';
         }
