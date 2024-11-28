a32a131b3501209b5943ebb486159abe494e1953
wijekim <130275773+wijekim@users.noreply.github.com>
2024-11-28 23:01:05 +0900

    Update two.cpp

--- 10-5/test1/two.cpp
+++ 10-5/test1/two.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    int a[2][2];
-    cout<<"첫번째 행렬값입력:";
-    for (int i = 0; i < 2; i++) {
+    int a[2][2]; //이차원 배열 생성
+    cout<<"첫번째 행렬값입력:"; 
+    for (int i = 0; i < 2; i++) { //행렬에 정수입력
         for (int j = 0; j < 2; j++) {
             cin >> a[i][j];
         }
@@ -12,8 +12,8 @@ int main() {
 
 
     int b[2][2];
-    cout<<"두번째 행렬값입력:";
-    for (int i = 0; i < 2; i++) {
+    cout<<"두번째 행렬값입력:"; 
+    for (int i = 0; i < 2; i++) { //행렬에 정수입력
         for (int j = 0; j < 2; j++) {
             cin >> b[i][j];
         }
@@ -21,7 +21,7 @@ int main() {
     cout<<endl;
 
 
-    int result[2][2];
+    int result[2][2]; //결과값 이차원 배열 생성
 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
@@ -30,7 +30,7 @@ int main() {
     }
     cout<<endl;
 
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++) { //a이차원 배열 출력
         for (int j = 0; j < 2; j++) {
             cout << a[i][j] << " ";
         }
@@ -38,7 +38,7 @@ int main() {
     }
     cout << endl;
 
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++) { //b이차원 배열 출력
         for (int j = 0; j < 2; j++) {
             cout << b[i][j] << " ";
         }
@@ -47,7 +47,7 @@ int main() {
     cout << endl;
 
     cout<<"연산결과:"<<endl;
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++) { //연산 결과값을 출력
         for (int j = 0; j < 2; j++) {
             cout << result[i][j] << " ";
         }
